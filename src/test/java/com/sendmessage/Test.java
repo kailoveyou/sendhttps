b540e4c4eecd56866fa17a90e50fc7336359f072
root <136849653@qq.com>
2019-05-10 17:52:03 +0800

    用于测试是否能成功提交

---
+++ src/test/java/com/sendmessage/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Test
+ * Author:   18811
+ * Date:     2019/5/10 17:50
+ * Description: 测试GitHub
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.sendmessage;
+
+public class Test {
+}
